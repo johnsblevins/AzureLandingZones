6c58e0f76d4957186baf85d9076869fe944fd4bf
John Blevins <joblevin@microsoft.com>
2021-04-10 09:48:42 -0400

    Update sub pipe

--- templates/entlz/scripts/entlz_prereqs.sh
+++ templates/entlz/scripts/entlz_prereqs.sh
@@ -3,7 +3,20 @@ az cloud set -n AzureUSGovernment
 az Login
 
 # Create Azure-Platform-Owners Azure AD Group
-platformOwnerGroup=$(az ad group create --display-name azure-platform-owners --mail-nickname azure-platform-owners --description "Members can Create EA Subs in the Default Management Group" --query objectId --output tsv)
+graphURI=$(az cloud show --query endpoints.microsoftGraphResourceId --output tsv)
+platformOwnerGroup=$(az rest --method post \
+    --url "${graphURI}beta/groups" \
+    --headers "{\"content-type\":\"application/json\"}" \
+    --body "{   \"description\": \"Members can Create EA Subs in the Default Management Group\", 
+                \"displayName\": \"azure-platform-owners\", 
+                \"isAssignableToRole\": \"false\", 
+                \"mailNickname\": \"azure-platform-owners\",
+                \"mailEnabled\": \"false\",
+                \"securityEnabled\": \"true\",
+                \"visibility\": \"Private\"
+            }" \
+    --query id \
+    --output tsv)
 
 # Create Azure AD App Registration and Service Principal
 appId=$(az ad app create --display-name "azure-entlz-deployer" --query appId --output tsv)
@@ -19,5 +32,14 @@ az ad group member add --group $platformOwnerGroup --member-id $objId
 
 # Assign Azure-Platform-Owners Group Owner role to Tenant Root Group Scope
 az role assignment create --role "Owner" --scope / --assignee $platformOwnerGroup
+
+# Assign azure-entlz-deployer Service Principal Global Reader role in AAD
+az rest --method post \
+    --url "${graphURI}beta/roleManagement/directory/roleAssignments" \
+    --headers "{\"content-type\":\"application/json\"}" \
+    --body "{   \"principalId\": \"$objId\", 
+                \"roleDefinitionId\": \"f2ef992c-3afb-46b9-b7cf-a126ee74c451\", 
+                \"directoryScopeId\": \"/\"
+            }"
 
 echo Done
