cfd7d8be1a7a634301ce5d038dfb74d59e185a0c
John Blevins <joblevin@microsoft.com>
2021-04-10 20:49:00 -0400

    Updated RBAC in Pipes

--- templates/entlz/scripts/entlz_prereqs.sh
+++ templates/entlz/scripts/entlz_prereqs.sh
@@ -2,21 +2,27 @@
 az cloud set -n AzureUSGovernment
 az Login
 
-# Create Azure-Platform-Owners Azure AD Group
-graphURI=$(az cloud show --query endpoints.microsoftGraphResourceId --output tsv)
-platformOwnerGroup=$(az rest --method post \
-    --url "${graphURI}beta/groups" \
-    --headers "{\"content-type\":\"application/json\"}" \
-    --body "{   \"description\": \"Members can Create EA Subs in the Default Management Group\", 
-                \"displayName\": \"azure-platform-owners\", 
-                \"isAssignableToRole\": \"false\", 
-                \"mailNickname\": \"azure-platform-owners\",
-                \"mailEnabled\": \"false\",
-                \"securityEnabled\": \"true\",
-                \"visibility\": \"Private\"
-            }" \
-    --query id \
-    --output tsv)
+# Create Azure-Platform-Owners Azure AD Group if not exists
+platformOwnerGroup=$(az ad group show -g azure-platform-owners --query objectId --output tsv)
+if [[ -z $platformOwnerGroup ]] ;
+then
+    graphURI=$(az cloud show --query endpoints.microsoftGraphResourceId --output tsv)
+    platformOwnerGroup=$(az rest --method post \
+        --url "${graphURI}beta/groups" \
+        --headers "{\"content-type\":\"application/json\"}" \
+        --body "{   \"description\": \"Members can Create EA Subs in the Default Management Group\", 
+                    \"displayName\": \"azure-platform-owners\", 
+                    \"isAssignableToRole\": \"false\", 
+                    \"mailNickname\": \"azure-platform-owners\",
+                    \"mailEnabled\": \"false\",
+                    \"securityEnabled\": \"true\",
+                    \"visibility\": \"Private\"
+                }" \
+        --query id \
+        --output tsv)
+else
+    echo "azure-platform-owners group already exists"
+fi;
 
 # Create Azure AD App Registration and Service Principal
 appId=$(az ad app create --display-name "azure-entlz-deployer" --query appId --output tsv)
@@ -38,7 +44,7 @@ az rest --method post \
     --url "${graphURI}beta/roleManagement/directory/roleAssignments" \
     --headers "{\"content-type\":\"application/json\"}" \
     --body "{   \"principalId\": \"$objId\", 
-                \"roleDefinitionId\": \"f2ef992c-3afb-46b9-b7cf-a126ee74c451\", 
+                \"roleDefinitionId\": \"fdd7a751-b60b-444a-984c-02652fe8fa1c\", 
                 \"directoryScopeId\": \"/\"
             }"
 
