e610989d9741501d9788865e57741bcd4ad27f5f
John Blevins <joblevin@microsoft.com>
2021-04-21 06:43:44 -0400

    Updated all pipes removed legacy

--- templates/entlz/_archive/es-vwan-template/cicd/deploy-es-hubspoke.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    $DeploymentName = "es-hubspoke-Deployment",
-    $TemplateURI = "https://raw.githubusercontent.com/johnsblevins/AzureLandingZones/master/templates/entscalelz/es-hubspoke-template/es-hubspoke.json",
-    $ParameterFile = "<local parameter file>" ,
-    $location = "usgovvirginia"
-)
-
-#Login-AzAccount
-New-AzTenantDeployment -name $DeploymentName -Location $location -TemplateUri $TemplateURI -TemplateParameterFile $ParameterFile
