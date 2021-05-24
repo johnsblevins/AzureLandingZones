c04421472b2a46de236f4ef4a74998a53069eb04
John Blevins <joblevin@microsoft.com>
2021-05-24 11:43:53 -0400

    Added mission landing zone

---
+++ templates/entlz/platform-connectivity-vnethubspoke-missionlz/src/scripts/config/append_prereq_endpoints.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+# append pre-req endpoints to an MLZ config file
+
+set -e
+
+error_log() {
+  echo "${1}" 1>&2;
+}
+
+usage() {
+  echo "append_prereq_endpoints.sh: append pre-req endpoints to an MLZ config file"
+  error_log "usage: append_prereq_endpoints.sh <file to append>"
+}
+
+if [[ "$#" -lt 1 ]]; then
+   usage
+   exit 1
+fi
+
+file_to_append=$1
+
+cloudEndpoints=($(az cloud show \
+  --query '[endpoints.resourceManager, suffixes.acrLoginServerEndpoint, suffixes.keyvaultDns, name, endpoints.activeDirectory]' \
+  --output tsv))
+
+append_if_not_empty() {
+  key_name=$1
+  key_value=$2
+  file=$3
+  if [[ $key_value ]]; then
+    printf "${key_name}=${key_value}\n" >> "${file}"
+  fi
+}
+
+append_if_not_empty "mlz_metadatahost" ${cloudEndpoints[0]} ${file_to_append}
+append_if_not_empty "mlz_acrLoginServerEndpoint" ${cloudEndpoints[1]} ${file_to_append}
+append_if_not_empty "mlz_keyvaultDns" ${cloudEndpoints[2]} ${file_to_append}
+append_if_not_empty "mlz_cloudname" ${cloudEndpoints[3]} ${file_to_append}
+append_if_not_empty "mlz_activeDirectory" ${cloudEndpoints[4]} ${file_to_append}
---
+++ templates/entlz/platform-connectivity-vnethubspoke-missionlz/src/scripts/terraform/destroy_terraform.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+# shellcheck disable=SC1090,SC1091,SC2154,SC2143
+# SC1090: Can't follow non-constant source. Use a directive to specify location.
+# SC1091: Not following. Shellcheck can't follow non-constant source.
+# SC2154: "var is referenced but not assigned". These values come from an external file.
+# SC2143: Use grep -q instead of comparing output. Ignored for legibility.
+#
+# Destroys a Terraform configuration given a backend configuration, a global variables file, and a terraform configurationd directory
+
+set -e
+
+if [[ "$#" -lt 2 ]]; then
+   echo "destroy_terraform.sh: initializes Terraform for a given directory using given a .env file for backend configuration"
+   echo "usage: destroy_terraform.sh <global variables file> <terraform configuration directory> <var_file> <auto approve (y/n)>"
+   exit 1
+fi
+
+globalvars=$(realpath "${1}")
+
+tf_dir=$(realpath "${2}")
+tf_name=$(basename "${3}")
+
+config_vars="${tf_dir}/config.vars"
+tfvars="${tf_dir}/${tf_name}"
+
+auto_approve=${4:-n}
+
+scripts_path=$(realpath "${BASH_SOURCE%/*}/..")
+
+# check for dependencies
+. "${scripts_path}/util/checkforazcli.sh"
+. "${scripts_path}/util/checkforterraform.sh"
+
+# Validate necessary Azure resources exist
+. "${scripts_path}/config/config_validate.sh" "${tf_dir}"
+
+# Validate configuration file exists
+. "${scripts_path}/util/checkforfile.sh" \
+   "${config_vars}" \
+   "The configuration file ${config_vars} is empty or does not exist. You may need to run MLZ setup."
+
+# Source configuration file
+. "${config_vars}"
+
+# Verify Service Principal is valid and set client_id and client_secret environment variables
+. "${scripts_path}/config/get_sp_identity.sh" "${config_vars}"
+
+# Set the terraform state key
+key="${mlz_env_name}${tf_name}"
+
+# initialize terraform in the configuration directory
+cd "${tf_dir}" || exit
+terraform init \
+   -backend-config "metadata_host=${metadata_host}" \
+   -backend-config "key=${key}" \
+   -backend-config "resource_group_name=${tf_be_rg_name}" \
+   -backend-config "storage_account_name=${tf_be_sa_name}" \
+   -backend-config "container_name=${container_name}" \
+   -backend-config "environment=${environment}" \
+   -backend-config "tenant_id=${tenant_id}" \
+   -backend-config "subscription_id=${sub_id}" \
+   -backend-config "client_id=${client_id}" \
+   -backend-config "client_secret=${client_secret}"
+
+# destroy the terraform configuration with global vars and the configuration's tfvars
+destroy_command="terraform destroy"
+
+if [[ $auto_approve == "y" ]]; then
+   destroy_command+=" -input=false -auto-approve"
+fi
+
+destroy_command+=" -var-file=${globalvars}"
+destroy_command+=" -var-file=${tfvars}"
+destroy_command+=" -var mlz_clientid=${client_id}"
+destroy_command+=" -var mlz_clientsecret=${client_secret}"
+destroy_command+=" -var mlz_objectid=${object_id}"
+
+eval "${destroy_command}"
